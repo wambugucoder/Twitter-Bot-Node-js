2afdf6806412ea0cc62b0047dcca4d7691b3634f
JosWambugu <josphatwambugu77@gmail.com>
2020-06-17 16:15:38 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:"KzlTHgdaI6cXvYFhUHrFUY2ra",
-  consumer_secret:"9rZMqSmWEog4Na9IPSKQcYlBGsm7OT6SkWC0dEPJHzTZidvreY",
-  access_token:"1205571465736790017-TAOCxoHP2ZsInpPoudpH0ZdQ7AFZYi",
-  access_token_secret:"18yrkYrsZlDiKRFWx6pM4hAhGihUwteqxFvVtL7CCMbDx"
+  consumer_key:"YOUR_KEY",
+  consumer_secret:"",
+  access_token:"",
+  access_token_secret:""
 };
